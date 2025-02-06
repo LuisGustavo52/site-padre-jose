9bf468c22253baa9ba84b6b935bc63b9b9b78fc7
Luís Gustavo <luisgustavo.teodoro52@gmail.com>
2025-02-05 21:58:34 -0300

    feat: Barra de navegação com offcanvas

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,11 @@ function loadContent(elementId, fileName) {
         .catch(error => console.error('Erro ao carregar o arquivo:', error));
 }
 
+function toggleOverlay() {
+    const overlay = document.getElementById("overlayMenu");
+    overlay.style.display = (overlay.style.display === "flex" ? "none" : "flex");
+}
+
 // Carregar a barra de navegação e o rodapé ao carregar a página
 document.addEventListener('DOMContentLoaded', () => {
     loadContent('main-header', 'navbar.html');
